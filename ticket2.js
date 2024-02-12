4fb7c50a9329e6dad003c9e5b063b63db59c5787
Tomas Dano <tdano1363@googlemail.com>
2024-02-12 22:00:25 +0600

    ftoc

---
+++ ticket2.js
@@ -0,0 +1,9 @@
+function convertFToC(fTemperature) {
+    let fTemp = - 32 * .5556;
+    return cTemperature + fTemp;
+}
+
+function convertCToF(cTemperature) {
+    let cTemp = - 32 / 1.8
+    return fTemperature * cTemp;
+}
