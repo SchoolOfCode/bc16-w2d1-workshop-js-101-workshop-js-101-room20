47a1de9b4ff8df09faf6fee7438b954162f2b759
moose-hub <infamollagames@gmail.com>
2024-02-12 16:28:19 +0000

    Created a small html form for converting currency from gbp to either EUR or USD

--- ticket1.js
+++ ticket1.js
@@ -7,3 +7,37 @@ function convertGBPtoUSD(gbpAmount) {
     let conversionRate = 1.5;
     return gbpAmount * conversionRate;
 }
+
+function convertGBPtoAny(gbpAmount, currencyCode) {
+    let conversionRate;
+    
+    if (currencyCode === "EUR") {
+        conversionRate = 1.25;
+    } else if (currencyCode === "USD") {
+        conversionRate = 1.5;
+    }
+    
+    return gbpAmount * conversionRate;
+}
+
+function convertCurrency() {
+    const convertAmountGBP = document.querySelector("#gbpAmount").value;
+    const currencyCode = document.querySelector("#currencyCode").value;
+    const resultShow = document.querySelector("#result");
+
+    let conversionRate;
+    let convertedAmount;
+
+    if (currencyCode === "EUR") {
+        conversionRate = 1.25;
+        convertedAmount = convertAmountGBP * conversionRate;
+    } else if (currencyCode === "USD") {
+        conversionRate = 1.5;
+        convertedAmount = convertAmountGBP * conversionRate;
+    } else {
+        resultShow.innerText = "Sorry, this currency is not supported";
+        return;
+    }
+
+    resultShow.innerText = convertedAmount;
+}
