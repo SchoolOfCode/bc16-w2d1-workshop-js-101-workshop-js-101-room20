3bec660cf12d23b36d4bd518667aa531a2e79bcd
Tomas Dano <tdano1363@googlemail.com>
2024-02-12 20:30:37 +0600

    EUR and USD

--- ticket1.js
+++ ticket1.js
@@ -7,3 +7,17 @@ function convertGBPtoUSD(gbpAmount) {
     let conversionRate = 1.5;
     return gbpAmount * conversionRate;
 }
+
+function convertGBPtoAny(GbpAmount, currencyCode) {
+    let conversionRate;
+
+    if (currencyCode === EUR) {
+    conversionRate = 1.25;
+    } else if (currencyCode === USD) {
+        conversionRate = 1.5;
+        return GbpAmount * conversionRate;
+    }
+
+    
+
+}
