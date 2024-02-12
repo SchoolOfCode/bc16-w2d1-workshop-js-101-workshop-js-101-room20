2976430022d3f0bd19ce64a4380a1df4e3f5209b
moose-hub <infamollagames@gmail.com>
2024-02-12 14:20:00 +0000

    Created function to convert GBP to Euro and Usd indidivudally

---
+++ ticket1.js
@@ -0,0 +1,9 @@
+function convertGBPtoEUR(gbpAmount) {
+    let conversionRate = 1.25;
+    return gbpAmount * conversionRate;
+}
+
+function convertGBPtoUSD(gbpAmount) {
+    let conversionRate = 1.5;
+    return gbpAmount * conversionRate;
+}
