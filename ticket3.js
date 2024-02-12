c8d6261a02b9be508edc4e3d0c38e01f2ebc9be2
Tomas Dano <tdano1363@googlemail.com>
2024-02-13 03:09:59 +0600

    discountapplied

--- ticket3.js
+++ ticket3.js
@@ -2,16 +2,20 @@ function applyDiscount(totalAmount) {
     let minSpend = 20; 
     let discountRate = 0.1; 
     let discount = totalAmount * discountRate;
+    let finalAmount;
 
     if (totalAmount > minSpend) {
         finalAmount = totalAmount - discount;
       return finalAmount;
     } else {
-        return totalAmount; 
-    }
-}
+        return totalAmount;
+        
 
-console.log(${applyDiscount(400)});
+        }
+}
+console.log(`With discount the price is ${applyDiscount(60)}`)
+
+
 
 
 
